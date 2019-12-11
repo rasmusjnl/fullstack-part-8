acdc2f539aa6c147b9b9d7392d8fd227ad40b92b
Rasmusl <lempinen.rasmus@gmail.com>
2019-12-11 19:14:56 +0200

    Ready for 8.8

---
+++ library-frontend/src/index.js
@@ -0,0 +1,25 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./App";
+
+import { ApolloClient } from "apollo-boost";
+import { ApolloProvider } from "@apollo/react-hooks";
+import { InMemoryCache } from "apollo-cache-inmemory";
+import { HttpLink } from "apollo-link-http";
+
+const cache = new InMemoryCache();
+const link = new HttpLink({
+  uri: "http://localhost:4000/"
+});
+
+const client = new ApolloClient({
+  cache,
+  link
+});
+
+ReactDOM.render(
+  <ApolloProvider client={client}>
+    <App />
+  </ApolloProvider>,
+  document.getElementById("root")
+);
